3bc9a4695fe1f41ef227dff0a75361e36ecae751
Valentyn <valantg@gmail.com>
2021-09-29 19:49:42 +0300

    check working eslint

--- task7/js/task-04.js
+++ task7/js/task-04.js
@@ -7,7 +7,7 @@ const incrementButton = document.querySelectorAll(
 )[0];
 
 let counterValue = Number(value.innerHTML);
-
+const incr
 const increment = () => (counterValue += 1);
 const decrement = () => (counterValue -= 1);
 const updateValue = () => (value.innerHTML = counterValue);
@@ -15,7 +15,7 @@ const updateValue = () => (value.innerHTML = counterValue);
 incrementButton.onclick = () => {
   increment();
   updateValue();
-};
+}
 
 decrementButton.onclick = () => {
   decrement();
